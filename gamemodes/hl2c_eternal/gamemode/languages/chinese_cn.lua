2ca6c37e3d89036d4e73676d577ad8a4a6036748
chickencvgmailcom <61649368+rain-rain-bob-sss@users.noreply.github.com>
2024-08-30 21:18:13 +0800

    2 langs
    cn-translated by my friend
    tw-based on cn

---
+++ gamemodes/hl2c_eternal/gamemode/languages/chinese_cn.lua
@@ -0,0 +1,228 @@
+--[[
+
+NO, IT WILL TAKE FOREVER TO TRANSLATE EVERYTHING.
+IT IS USED FOR SKILL DESCRIPTIONS FOR NOW.
+--then i'll do it --[TW]Rain_bob
+
+]]
+
+translate.AddLanguage("zh-CN", "Chinese-CN")
+
+LANG.Defense = "防御"
+LANG.Gunnery = "炮兵"
+LANG.Medical = "医疗"
+LANG.Surgeon = "外科医生"
+LANG.Vitality = "活力"
+LANG.Knowledge = "知识"
+LANG.BetterEngine = "更好的引擎"
+
+LANG.Gunnery_d="枪械伤害+1%"
+LANG.Gunnery_ed="+3% 枪械伤害\n枪械等级 15 以上：非枪械每点伤害 +2.5%”
+
+LANG.Defense_d="+0.8% 敌方子弹伤害抵抗力" 
+LANG.Defense_ed="+2.5% 敌方子弹伤害抵抗力\n15 级以上：+2% 除枪支外所有来源的伤害抵抗力"
+
+LANG.Medical_d="医疗包效果+2%"
+LANG.Medical_ed="医疗包效果+5%"
+
+LANG.Surgeon_d="+2% 医疗包最大弹药量 / +2% 医疗包回血速度增加" 
+LANG.Surgeon_ed="+10% 医疗包最大弹药量 / +10% 医疗包回血速度增加"
+
+LANG.Vitality_d="+1 健康值"
+LANG.Vitality_ed="+5生命值"
+
+LANG.Knowledge_d="+3%经验值增益"
+LANG.Knowledge_ed="+5%经验值加成"
+
+LANG.BetterEngine_d='+4 马力给你生成的车辆'
+LANG.BetterEngine_ed='您生成的车辆的 +8 马力。\n-4% 加成延迟\n+0.05% 最大速''
+
+LANG.hpboost_1='生命值提升''
+LANG.hpboost_1_d='生命值提高 15''
+LANG.hpboost_1_ed='生命值提高 85''
+
+LANG.dmgboost_1='伤害加成''
+LANG.dmgboost_1_d='+6% 伤害''
+LANG.dmgboost_1_ed='+47% 伤害''
+
+LANG.damageresistanceboost_1="伤害抗性提升"
+LANG.damageresistanceboost_1_d="提升 7% 的伤害抗性"
+LANG.damageresistanceboost_1_ed="提升 57% 的伤害抗性"
+
+LANG.antipoison_1="防毒"
+LANG.antipoison_1_d="减少一半来自剧毒猎头蟹的伤害（最多减少 25 伤害）"
+LANG.antipoison_2_ed="减少一半剧毒猎头蟹造成的伤害（最多减少 100 点伤害）"
+
+LANG.critical_damage_1="临界伤害 I"
+LANG.critical_damage_1_d="7%几率造成 1.2 倍伤害"
+LANG.critical_damage_1_ed="12%几率造成 2.2 倍伤害"
+
+LANG.better_knowledge_1="经验值增益"
+LANG.better_knowledge_1_d="击杀 NPC 获得 +40% xp 增益"
+LANG.better_knowledge_1_ed="如果难度高于 650%，击杀 NPC 可获得 2.35 倍 xp 增益.\n+10% 地图完成时的奖励 xp 提升，知识技能每点 +2% xp 增益"
+
+LANG.vampiric_killer_1="吸血"
+LANG.vampiric_killer_1_d="杀死一名 NPC 时，您将获得 +2 HP。"
+LANG.vampiric_killer_1_ed="杀死 NPC 时，您将获得 +4% 的生命值。最多恢复 50 HP。"
+
+LANG.critical_damage_2='暴击伤害 II''
+LANG.critical_damage_2_d='什么都不做。”
+LANG.critical_damage_2_ed='提高暴击伤害 I 特性，几率 12% -> 19% 和伤害 2.2 倍 -> 2.5 倍。\n还提供 6% 的几率造成超级暴击，提供 4 倍伤害！''
+
+LANG.NoSpam = "不要发送垃圾邮件"
+
+LANG.AllowedVehSpawn="允许生成车辆！按 F3（备用 1）可生成车辆"。
+LANG.SpawnVehicle = "生成车辆"
+LANG.NoSpaceForSpawnVeh="没有足够空间生成车辆！"
+LANG.SpawnVehPlayerNear="您周围有玩家！寻找一个空旷的空间来生成您的车辆。"
+LANG.SpawnVehNotAllowed="您现在不能生成车辆。"
+LANG.RemoveVehNotAllowed="此时您不能移除车辆。"
+
+LANG.AllPlayersDied="所有玩家都已死亡！" 
+LANG.DiedBy="被 #%s 杀死"
+
+LANG.hl2="半条命 2" 
+LANG.hl2_ep1="半条命 2：第一章" 
+LANG.hl2_ep2="半条命 2：第二章"
+
+LANG.CompletedCampaign="恭喜 - 您已完成 %s。" 
+LANG.AwardedXP="您被授予 %d XP" 
+LANG.LostXP="丢失 %d XP。"
+
+LANG.NotAdmin="你不是管理员！" 
+LANG.NotDead="你没死！" 
+LANG.CantRespawnWhileChangingMap="地图正在改变，你现在无法重生！"
+
+LANG.ImportantNPCKilledByYou="您杀死了一名重要的 NPC 角色！"
+LANG.ImportantNPCKilledByOther="%s杀死了一名重要的NPC演员！"
+LANG.ImportantNPCDied="重要 NPC 角色已死亡！"
+
+LANG.LVLMaxed="等级已达上限。您必须获得声望才能更进一步。"
+LANG.LVLIncreased="等级提高：%i --> %i"
+
+LANG.PrestigeMaxed="声望已满。永恒更进一步"。
+LANG.PrestigeIncreased="声望增加！(%i --> %i)"
+LANG.PrestigeFirstTime="%s首次获得声望！"
+
+LANG.EternityIncreased="等级增加！(%i --> %i)"
+LANG.EternitiesMaxed="你的等级已达到上限。您必须进行天体化才能获得更多等级!。"
+
+LANG.PetsLocked="你还没有解锁宠物！"
+
+LANG.scoreboard_singleplayer="1 玩家"
+LANG.scoreboard_players="%d 玩家"
+
+LANG.alive="活着"
+LANG.completedmap="已完成地图"
+LANG.dead="死亡" 
+
+LANG.ObjectiveTimeLeft=function(timeleftsec,timeleftmin,timeleft)
+    return timeleftsec <= 0 and "Objective: Complete the map within "..timeleftmin.." minutes! (Time left: "..math.floor(timeleft - CurTime()).."s)" or "Objective: Complete the map within "..timeleftmin.." minutes and "..timeleftsec.." seconds! (Time left: "..math.floor(timeleft - CurTime()).."s)"
+end
+
+LANG.Difficulty="难度：%s%%"
+LANG.DifficultyTotal="%s%% 总计"
+
+LANG.Health="生命值：%s/%s (%d%%)"
+LANG.Armor="护甲：%s/%s (%d%%)"
+
+LANG.NextMapIn="下一张地图在 %s"
+LANG.SwitchingMap="切换地图！"
+
+LANG.RestartMapIn="在 %s 中重启地图"
+LANG.Restarting="重启地图！"
+
+LANG.XPGained="%s XP gained"
+LANG.TotalXPGained="(%s XP gained total)"
+
+LANG.Help="帮助"
+LANG.HelpText="-= 关于这个游戏模式 =-\n欢迎来到《半条命2》战役EX模式！这个游戏模式基于Jai 'Choccy' Fox制作的《半条命2》战役，\n加入了升级、技能等新内容！\n[F3] （Spare 1） - 在允许的情况下生成一辆车。\n[F4] （备用 2） - 移除一辆车（如果你有的话）。
+LANG.HelpEXModeOn="EX 模式已启用！期待这里的地图目标、NPC 变异和混乱！"
+LANG.HelpEXModeOff="EX 模式已禁用！"
+LANG.HelpEndlessOn="（无尽模式已启用。难度上限大幅提高。进度最终会变成指数级。"
+LANG.HelpEndlessOff="/nEndless 模式已禁用。难度有限，技能和 Perks 功能有限。"
+
+LANG.AMode="管理模式"
+
+LANG.AModeOn="已启用"
+LANG.AModeOff="禁用"
+
+LANG.UnspentSP="未消耗的技能点数：%s"
+LANG.SpendDesiredSP="右键单击以在技能上花费所需的 SP 值"
+LANG.SpendAllSP="按住 SHIFT 的同时单击可将所有 SP 花在所需技能上"
+LANG.YourSkills="您的技能"
+LANG.SkillIncrease="增加 %s"
+
+LANG.Options="选项"
+LANG.Option_DisableTinnitus="禁用耳鸣/耳鸣"
+LANG.Option_DisableTinnitusToolTip="在受到爆炸伤害时禁用恼人的耳鸣声"
+
+LANG.XP="XP：%s / %s (%s%%)"
+LANG.Level="等级：%s"
+LANG.SPs="技能点数：%s"
+
+LANG.NonEndlessDesc="/n\nIn Non-Endless Mode:\n"
+LANG.EndlessDesc="/n\nIn Endless Mode:\n"
+
+LANG.Prestige="Prestige: %s"
+LANG.PrestigePerkDesc="Prestige Perks to give you more advantage"
+LANG.PrestigeTxt="Prestige"
+LANG.PrestigePoints="声望点数：%s"
+LANG.PrestigeNotEnough="声望不够"
+LANG.Eternities="永恒：%s"
+LANG.Eternity="永恒"
+LANG.EternityPerkDesc="永恒特权。它们要强大得多"。
+LANG.CelestialityPoints="天体点数：%s"
+LANG.Celestiality="天体"
+LANG.CelestialityPerkDesc="天体特权"
+LANG.PointsCost="点数成本：%s"
+LANG.PrestigeCost="声望需求：%s"
+
+LANG.prestige_text1="声望将重置你的所有等级、XP 和技能，但你将获得 +25% 的 xp 增益（每个声望）和一个装备点。
+LANG.prestige_text2="您必须达到 %s 级，并达到下一级的最大 XP 才能获得声望。"
+LANG.prestige_text3="首次获得声望将永久性地将技能点数收益提高至每级 2 点，并将技能最高等级提高至 35 级。"
+
+LANG.eternity_text1="永恒重置你的等级、XP、技能、声望和声望福利，但你的 XP 增益（每永恒一次）和永恒点数将提升 +175%。永恒福利比普通福利更强大"。
+LANG.eternity_text2="必须达到下一级所需的最大 xp 值，在 %s 威望值下达到 %s 等级，才能获得永恒"
+
+LANG.Inf="无限"
+
+LANG.Unlock="解锁"
+
+LANG.Playermodels="游戏模型"
+LANG.Refreshstats="刷新统计"
+LANG.Skills="技能"
+LANG.Perks="装备"
+
+LANG.FP_text1="Ah a fellow player."
+LANG.FP_text2="恭喜获得声望"。
+LANG.FP_text3="你似乎开始了解这个游戏模式的机制了"
+LANG.FP_text4="%s？这名字不错"
+LANG.FP_text5="我们的名字是......"
+LANG.FP_notimetolose="Gordon！我们没时间了 We got no time to lose！我们必须继续前进！"
+LANG.FP_perksunlock="Perks unlocked."
+LANG.FP_bonuslvl="每提升一级可获得 2 点技能点数，技能最高等级提升至 35 级。"
+
+LANG.UP_NEEDSP="您需要技能点数才能升级此技能！"
+LANG.UP_MAXED="您已达到此技能的最大点数！"
+LANG.UP_INCREASED="增加了 %s %i 点！"
+LANG.UP_UNLOCKPERK="已解锁装备：%s"
+LANG.UP_NOTENOUGH="没有足够的%s"
+LANG.UP_NOTENOUGHPOINTS="没有足够的 %s 点数！"
+
+LANG.NV_DAMAGEDBYAGV="您正被防蚁守卫变体伤害，远离它！"
+
+LANG.medkit_purpose="用你的主要攻击治愈他人。"
+LANG.medkit_instructions="每点医疗技能效果提高 2%，最大效率为 120%。记住，治疗其他玩家将使你获得所治疗健康值的 1/4！"
+
+LANG.fm_fool="你这个傻瓜！"
+LANG.fm_timedout="超时了！"
+
+LANG.CP="%s已激活检查点！"
+LANG.MINICP="%s已激活迷你检查点！"
+
+LANG.AFK="你挂机太久辣!"
+
+LANG.Dead_Chat="*死亡* "
+LANG.TEAM_Chat=" (团队) "
+LANG.Console="控制台"
---
+++ gamemodes/hl2c_eternal/gamemode/languages/chinese_tw.lua
@@ -0,0 +1,228 @@
+--[[
+
+NO, IT WILL TAKE FOREVER TO TRANSLATE EVERYTHING.
+IT IS USED FOR SKILL DESCRIPTIONS FOR NOW.
+--then i'll do it --[TW]Rain_bob
+
+]]
+
+translate.AddLanguage("zh-TW", "Chinese-TW")
+
+LANG.Defense = "防御"
+LANG.Gunnery = "炮兵"
+LANG.Medical = "醫療"
+LANG.Surgeon = "外科醫生"
+LANG.Vitality = "活力"
+LANG.Knowledge = "知識"
+LANG.BetterEngine = "更好的引擎"
+
+LANG.Gunnery_d="槍械傷害+1%"
+LANG.Gunnery_ed="+3% 槍械傷害\n槍械等級 15 以上：非槍械每點傷害 +2.5%”
+
+LANG.Defense_d="+0.8% 敵方子彈傷害抵抗力" 
+LANG.Defense_ed="+2.5% 敵方子彈傷害抵抗力\n15 級以上：+2% 除槍支外所有來源的傷害抵抗力"
+
+LANG.Medical_d="醫療包效果+2%"
+LANG.Medical_ed="醫療包效果+5%"
+
+LANG.Surgeon_d="+2% 醫療包最大彈藥量 / +2% 醫療包回血速度增加" 
+LANG.Surgeon_ed="+10% 醫療包最大彈藥量 / +10% 醫療包回血速度增加"
+
+LANG.Vitality_d="+1 健康值"
+LANG.Vitality_ed="+5生命值"
+
+LANG.Knowledge_d="+3%經驗值增益"
+LANG.Knowledge_ed="+5%經驗值加成"
+
+LANG.BetterEngine_d='+4 馬力給你生成的車輛'
+LANG.BetterEngine_ed='您生成的車輛的 +8 馬力。\n-4% 加成延遲\n+0.05% 最大速''
+
+LANG.hpboost_1='生命值提升''
+LANG.hpboost_1_d='生命值提高 15''
+LANG.hpboost_1_ed='生命值提高 85''
+
+LANG.dmgboost_1='傷害加成''
+LANG.dmgboost_1_d='+6% 傷害''
+LANG.dmgboost_1_ed='+47% 傷害''
+
+LANG.damageresistanceboost_1="傷害抗性提升"
+LANG.damageresistanceboost_1_d="提升 7% 的傷害抗性"
+LANG.damageresistanceboost_1_ed="提升 57% 的傷害抗性"
+
+LANG.antipoison_1="防毒"
+LANG.antipoison_1_d="減少一半來自劇毒獵頭蟹的傷害（最多減少 25 傷害）"
+LANG.antipoison_2_ed="減少一半劇毒獵頭蟹造成的傷害（最多減少 100 點傷害）"
+
+LANG.critical_damage_1="臨界傷害 I"
+LANG.critical_damage_1_d="7%幾率造成 1.2 倍傷害"
+LANG.critical_damage_1_ed="12%幾率造成 2.2 倍傷害"
+
+LANG.better_knowledge_1="經驗值增益"
+LANG.better_knowledge_1_d="擊殺 NPC 獲得 +40% xp 增益"
+LANG.better_knowledge_1_ed="如果難度高於 650%，擊殺 NPC 可獲得 2.35 倍 xp 增益.\n+10% 地圖完成時的獎勵 xp 提升，知識技能每點 +2% xp 增益"
+
+LANG.vampiric_killer_1="吸血"
+LANG.vampiric_killer_1_d="殺死一名 NPC 時，您將獲得 +2 HP。"
+LANG.vampiric_killer_1_ed="殺死 NPC 時，您將獲得 +4% 的生命值。最多恢覆 50 HP。"
+
+LANG.critical_damage_2='暴擊傷害 II''
+LANG.critical_damage_2_d='什麽都不做。”
+LANG.critical_damage_2_ed='提高暴擊傷害 I 特性，幾率 12% -> 19% 和傷害 2.2 倍 -> 2.5 倍。\n還提供 6% 的幾率造成超級暴擊，提供 4 倍傷害！''
+
+LANG.NoSpam = "不要發送垃圾郵件"
+
+LANG.AllowedVehSpawn="允許生成車輛！按 F3（備用 1）可生成車輛"。
+LANG.SpawnVehicle = "生成車輛"
+LANG.NoSpaceForSpawnVeh="沒有足夠空間生成車輛！"
+LANG.SpawnVehPlayerNear="您周圍有玩家！尋找一個空曠的空間來生成您的車輛。"
+LANG.SpawnVehNotAllowed="您現在不能生成車輛。"
+LANG.RemoveVehNotAllowed="此時您不能移除車輛。"
+
+LANG.AllPlayersDied="所有玩家都已死亡！" 
+LANG.DiedBy="被 #%s 殺死"
+
+LANG.hl2="半條命 2" 
+LANG.hl2_ep1="半條命 2：第一章" 
+LANG.hl2_ep2="半條命 2：第二章"
+
+LANG.CompletedCampaign="恭喜 - 您已完成 %s。" 
+LANG.AwardedXP="您被授予 %d XP" 
+LANG.LostXP="丟失 %d XP。"
+
+LANG.NotAdmin="你不是管理員！" 
+LANG.NotDead="你沒死！" 
+LANG.CantRespawnWhileChangingMap="地圖正在改變，你現在無法重生！"
+
+LANG.ImportantNPCKilledByYou="您殺死了一名重要的 NPC 角色！"
+LANG.ImportantNPCKilledByOther="%s殺死了一名重要的NPC演員！"
+LANG.ImportantNPCDied="重要 NPC 角色已死亡！"
+
+LANG.LVLMaxed="等級已達上限。您必須獲得聲望才能更進一步。"
+LANG.LVLIncreased="等級提高：%i --> %i"
+
+LANG.PrestigeMaxed="聲望已滿。永恒更進一步"。
+LANG.PrestigeIncreased="聲望增加！(%i --> %i)"
+LANG.PrestigeFirstTime="%s首次獲得聲望！"
+
+LANG.EternityIncreased="等級增加！(%i --> %i)"
+LANG.EternitiesMaxed="你的等級已達到上限。您必須進行天體化才能獲得更多等級!。"
+
+LANG.PetsLocked="你還沒有解鎖寵物！"
+
+LANG.scoreboard_singleplayer="1 玩家"
+LANG.scoreboard_players="%d 玩家"
+
+LANG.alive="活著"
+LANG.completedmap="已完成地圖"
+LANG.dead="死亡" 
+
+LANG.ObjectiveTimeLeft=function(timeleftsec,timeleftmin,timeleft)
+    return timeleftsec <= 0 and "Objective: Complete the map within "..timeleftmin.." minutes! (Time left: "..math.floor(timeleft - CurTime()).."s)" or "Objective: Complete the map within "..timeleftmin.." minutes and "..timeleftsec.." seconds! (Time left: "..math.floor(timeleft - CurTime()).."s)"
+end
+
+LANG.Difficulty="難度：%s%%"
+LANG.DifficultyTotal="%s%% 總計"
+
+LANG.Health="生命值：%s/%s (%d%%)"
+LANG.Armor="護甲：%s/%s (%d%%)"
+
+LANG.NextMapIn="下一張地圖在 %s"
+LANG.SwitchingMap="切換地圖！"
+
+LANG.RestartMapIn="在 %s 中重啟地圖"
+LANG.Restarting="重啟地圖！"
+
+LANG.XPGained="%s XP gained"
+LANG.TotalXPGained="(%s XP gained total)"
+
+LANG.Help="幫助"
+LANG.HelpText="-= 關於這個遊戲模式 =-\n歡迎來到《半條命2》戰役EX模式！這個遊戲模式基於Jai 'Choccy' Fox制作的《半條命2》戰役，\n加入了升級、技能等新內容！\n[F3] （Spare 1） - 在允許的情況下生成一輛車。\n[F4] （備用 2） - 移除一輛車（如果你有的話）。
+LANG.HelpEXModeOn="EX 模式已啟用！期待這里的地圖目標、NPC 變異和混亂！"
+LANG.HelpEXModeOff="EX 模式已禁用！"
+LANG.HelpEndlessOn="（無盡模式已啟用。難度上限大幅提高。進度最終會變成指數級。"
+LANG.HelpEndlessOff="/nEndless 模式已禁用。難度有限，技能和 Perks 功能有限。"
+
+LANG.AMode="管理模式"
+
+LANG.AModeOn="已啟用"
+LANG.AModeOff="禁用"
+
+LANG.UnspentSP="未消耗的技能點數：%s"
+LANG.SpendDesiredSP="右鍵單擊以在技能上花費所需的 SP 值"
+LANG.SpendAllSP="按住 SHIFT 的同時單擊可將所有 SP 花在所需技能上"
+LANG.YourSkills="您的技能"
+LANG.SkillIncrease="增加 %s"
+
+LANG.Options="選項"
+LANG.Option_DisableTinnitus="禁用耳鳴/耳鳴"
+LANG.Option_DisableTinnitusToolTip="在受到爆炸傷害時禁用惱人的耳鳴聲"
+
+LANG.XP="XP：%s / %s (%s%%)"
+LANG.Level="等級：%s"
+LANG.SPs="技能點數：%s"
+
+LANG.NonEndlessDesc="/n\nIn Non-Endless Mode:\n"
+LANG.EndlessDesc="/n\nIn Endless Mode:\n"
+
+LANG.Prestige="Prestige: %s"
+LANG.PrestigePerkDesc="Prestige Perks to give you more advantage"
+LANG.PrestigeTxt="Prestige"
+LANG.PrestigePoints="聲望點數：%s"
+LANG.PrestigeNotEnough="聲望不夠"
+LANG.Eternities="永恒：%s"
+LANG.Eternity="永恒"
+LANG.EternityPerkDesc="永恒特權。它們要強大得多"。
+LANG.CelestialityPoints="天體點數：%s"
+LANG.Celestiality="天體"
+LANG.CelestialityPerkDesc="天體特權"
+LANG.PointsCost="點數成本：%s"
+LANG.PrestigeCost="聲望需求：%s"
+
+LANG.prestige_text1="聲望將重置你的所有等級、XP 和技能，但你將獲得 +25% 的 xp 增益（每個聲望）和一個裝備點。
+LANG.prestige_text2="您必須達到 %s 級，並達到下一級的最大 XP 才能獲得聲望。"
+LANG.prestige_text3="首次獲得聲望將永久性地將技能點數收益提高至每級 2 點，並將技能最高等級提高至 35 級。"
+
+LANG.eternity_text1="永恒重置你的等級、XP、技能、聲望和聲望福利，但你的 XP 增益（每永恒一次）和永恒點數將提升 +175%。永恒福利比普通福利更強大"。
+LANG.eternity_text2="必須達到下一級所需的最大 xp 值，在 %s 威望值下達到 %s 等級，才能獲得永恒"
+
+LANG.Inf="無限"
+
+LANG.Unlock="解鎖"
+
+LANG.Playermodels="遊戲模型"
+LANG.Refreshstats="刷新統計"
+LANG.Skills="技能"
+LANG.Perks="裝備"
+
+LANG.FP_text1="Ah a fellow player."
+LANG.FP_text2="恭喜獲得聲望"。
+LANG.FP_text3="你似乎開始了解這個遊戲模式的機制了"
+LANG.FP_text4="%s？這名字不錯"
+LANG.FP_text5="我們的名字是......"
+LANG.FP_notimetolose="Gordon！我們沒時間了 We got no time to lose！我們必須繼續前進！"
+LANG.FP_perksunlock="Perks unlocked."
+LANG.FP_bonuslvl="每提升一級可獲得 2 點技能點數，技能最高等級提升至 35 級。"
+
+LANG.UP_NEEDSP="您需要技能點數才能升級此技能！"
+LANG.UP_MAXED="您已達到此技能的最大點數！"
+LANG.UP_INCREASED="增加了 %s %i 點！"
+LANG.UP_UNLOCKPERK="已解鎖裝備：%s"
+LANG.UP_NOTENOUGH="沒有足夠的%s"
+LANG.UP_NOTENOUGHPOINTS="沒有足夠的 %s 點數！"
+
+LANG.NV_DAMAGEDBYAGV="您正被防蟻守衛變體傷害，遠離它！"
+
+LANG.medkit_purpose="用你的主要攻擊治愈他人。"
+LANG.medkit_instructions="每點醫療技能效果提高 2%，最大效率為 120%。記住，治療其他玩家將使你獲得所治療健康值的 1/4！"
+
+LANG.fm_fool="你這個傻瓜！"
+LANG.fm_timedout="超時了！"
+
+LANG.CP="%s已激活檢查點！"
+LANG.MINICP="%s已激活迷你檢查點！"
+
+LANG.AFK="你掛機太久辣!"
+
+LANG.Dead_Chat="*死亡* "
+LANG.TEAM_Chat=" (團隊) "
+LANG.Console="控制台"
